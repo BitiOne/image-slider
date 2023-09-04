c339bd3969c5936ee512dbafa42d02fdda59ab2d
Krisztián Bittner <bittner.school@gmail.com>
2023-09-04 16:50:40 +0200

    JS started.

---
+++ script.js
@@ -0,0 +1,5 @@
+const bigImageContainer = document.querySelector('#big-image');
+const smallImagesMainContainer = document.querySelector('#small-images-main-container');
+const imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg'];
+
+let activeImage = 1;
